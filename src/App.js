e0cd90f0e1deaabe650be658b26a74197e08ff78
SMAK-AXOLOTL <vpirovskiy@gmail.com>
2023-06-29 20:19:57 +0300

    An ongoing fight with git - in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, {useEffect} from "react";
+import React, {lazy, Suspense, useEffect} from "react";
 import s from './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MusicContainer from "./components/Music/MusicContainer";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
-import MessagesContainer from "./components/Messages/MessagesContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
 import Redirector from "./components/Common/Redirector";
 import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import store from "./redux/reduxStore";
 
-
+const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer.jsx"))
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer.jsx"))
+const MessagesContainer = lazy(() => import('./components/Messages/MessagesContainer'))
+const Login = lazy(() => import("./components/Login/Login"))
 const App = props => {
 
     useEffect(() => {
@@ -33,25 +32,25 @@ const App = props => {
                 <HeaderContainer/>
                 <Navbar dialogs={props.dialogsData}/>
                 <div className='app_wrapper_content'>
-                    <Routes>
-                        <Route path='/messages/*' element={
-                            <MessagesContainer store={props.store}/>
-                        }/>
-                        <Route path='/profile/:userId?' element={
-                            <ProfileContainer/>
-                        }/>
-                        <Route path='/music' element={<MusicContainer/>}/>
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/users' element={<UsersContainer/>}/>
-                        <Route path='/music' element={<MusicContainer/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-                        <Route path='/login' element={<Login/>}/>
-                        <Route path='' element={<Redirector to={'/profile'}/>}/>
-                    </Routes>
+                    <Suspense fallback={<div><Preloader/></div>}>
+                        <Routes>
+                            <Route path='/messages/*' element={<MessagesContainer />}/>
+                            <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+                            <Route path='/music' element={<MusicContainer/>}/>
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='/users' element={<UsersContainer/>}/>
+                            <Route path='/music' element={<MusicContainer/>}/>
+                            <Route path='/settings' element={<Settings/>}/>
+
+                            <Route path='/login' element={<Login/>}/>
+                            <Route path='' element={<Redirector to={'/profile'}/>}/>
+                        </Routes>
+                    </Suspense>
                 </div>
             </div>
         </Provider>
     </BrowserRouter>
+
 }
 
 const mapStateToProps = (state) => ({
--- src/components/Common/Preloader/Preloader.jsx
+++ src/components/Common/Preloader/Preloader.jsx
@@ -3,7 +3,7 @@ import s from "./Preloader.module.css"
 import preloader from "../../../assets/gifs/123.gif"
 
 let Preloader = (props) => {
-    return <div>
+    return <div className={s.container}>
         <img src={preloader}/>
     </div>
 }
